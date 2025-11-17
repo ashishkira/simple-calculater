3ea4d9f60fc9648d4792f7d1de05ad3be8166f56
ashishkira <ashishsingh0p768@gmail.com>
2025-11-17 23:17:18 +0530

    msg

---
+++ calculate.py
@@ -0,0 +1,26 @@
+
+def main():
+    while go == "go":
+        number1 = int(input("Enter your fist number : "))
+        oparator = input("Enter you oparator : ")
+        number2 = int(input("Enter your second name : "))
+        
+        if oparator == "+":
+            result = number1 + number2
+            print(result)
+        elif oparator == "-":
+            result = number1 - number2
+            print(result)
+        elif oparator == "*":
+            result = number1 * number2
+            print(result)
+        elif oparator == "/":
+            result = number1 / number2
+            print(result)
+        else:
+            print("bye..")
+            break
+
+go = "go"
+
+main()
